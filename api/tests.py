942e89dfb9625401d9c7f489b9b71c5b86e11b47
Charliiee <Charliiee@users.noreply.github.com>
2019-05-31 20:32:56 -0300

    :white_check_mark: Add Employee API view tests

--- api/tests.py
+++ api/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 from django.db.utils import IntegrityError
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
 
 from api.models import Employee
+from api.serializers import EmployeeSerializer
 
 
 class EmployeeModelTests(TestCase):
-
     """Test class for Employee model."""
 
     def test_employee_unique_email(self):
@@ -38,3 +40,38 @@ class EmployeeModelTests(TestCase):
 
         names = sorted(['John Doe', 'Jane Doe', 'Richard Roe'])
         self.assertEqual(list(Employee.objects.values_list('name', flat=True)), names)
+
+
+class EmployeeAPIViewTests(APITestCase):
+    """Test class for EmployeeView class."""
+
+    def setUp(self):
+        """
+        Set up necessary objects for testing this class.
+        """
+        super().setUp()
+
+        Employee.objects.bulk_create([
+            Employee(name='John Doe',
+                     email='john.doe@luizalabs.com',
+                     department='Development'),
+            Employee(name='Jane Doe',
+                     email='jane.doe@luizalabs.com',
+                     department='Marketing'),
+            Employee(name='Richard Roe',
+                     email='richard.roe@luizalabs.com',
+                     department='Sales'),
+        ])
+
+    def test_employee_basic_request(self):
+        """
+        employee_basic_request returns True if Employee response
+        retrieves all employees.
+        """
+        response = self.client.get(reverse('employee-list'))
+        self.assertEqual(200, response.status_code)
+
+        employees_serialized_data = EmployeeSerializer(instance=Employee.objects.all(), many=True).data
+
+        response_data = response.json()
+        self.assertEqual(employees_serialized_data, response_data)
