910a89b4031c3e593a58bd2799e3116a042aa1c0
Charliiee <Charliiee@users.noreply.github.com>
2019-05-31 19:14:02 -0300

    Add Employee model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    """Employee model represents employees at Luizalabs."""
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    department = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        """Override save method to convert Employee's name to title before saving."""
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "{} at {}".format(self.name, self.department)
+
+    class Meta:
+        ordering = ('name',)
