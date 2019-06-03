d054ee7ec0617a2b297dd9d4993006048d3e02fd
Charliiee <Charliiee@users.noreply.github.com>
2019-06-03 13:59:55 -0300

    Add employee filter to requests

--- api/views.py
+++ api/views.py
@@ -11,3 +11,25 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     """
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the employees for the
+        filtered by the fields specified in the request
+        """
+        employees = Employee.objects.all()
+
+        name = self.request.query_params.get('name')
+        email = self.request.query_params.get('email')
+        department = self.request.query_params.get('department')
+
+        if name is not None:
+            employees = employees.filter(name__icontains=name)
+
+        if email is not None:
+            employees = employees.filter(email__icontains=email)
+
+        if department is not None:
+            employees = employees.filter(department__icontains=department)
+
+        return employees
