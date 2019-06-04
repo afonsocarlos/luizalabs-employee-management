9cc1f0ff4c29c4b21ecaec1cc6d5cc8b29c760c5
Charliiee <Charliiee@users.noreply.github.com>
2019-06-04 08:52:27 -0300

    Add djangorestframework basic settings
    Set pagination to requests

--- luizalabs_employee_management/settings.py
+++ luizalabs_employee_management/settings.py
@@ -85,6 +85,21 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': (
+         'rest_framework.pagination.LimitOffsetPagination'
+    ),
+
+    'PAGE_SIZE': 100,
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
