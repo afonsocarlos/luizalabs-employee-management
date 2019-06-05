959a093f9632d9bbaa36dae3f530289525a19e05
Charliiee <Charliiee@users.noreply.github.com>
2019-06-05 01:26:19 -0300

    Change jwt tokwn expiration time to 1 day

--- luizalabs_employee_management/settings.py
+++ luizalabs_employee_management/settings.py
@@ -104,9 +104,8 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
     'JWT_ALLOW_REFRESH': True,
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(hours=1),
 }
 
 # Password validation
